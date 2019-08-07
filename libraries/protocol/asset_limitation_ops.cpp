50065329a4ca85d4968a87252d1dba48dee9d995
DmitriyShalaev <shalaevdmitriy99@gmail.com>
2019-08-07 19:04:20 +0300

    asset_limitation_ops for sell and buy limitation , issue to delete meta1_limitation from asset_object

---
+++ libraries/protocol/asset_limitation_ops.cpp
@@ -0,0 +1,33 @@
+#include <graphene/protocol/asset_limitation_ops.hpp>
+
+namespace graphene
+{
+namespace protocol
+{
+
+void asset_limitation_options::validate() const
+{
+    FC_ASSERT(!sell_limit.empty());
+    FC_ASSERT(!buy_limit.empty());
+}
+
+void asset_limitation_object_create_operation::validate() const
+{
+    FC_ASSERT(fee.amount >= 0);
+    common_options.validate();
+}
+
+void asset_limitation_object_update_operation::validate() const
+{
+    FC_ASSERT(fee.amount >= 0);
+    new_options.validate();
+}
+
+} // namespace protocol
+} // namespace graphene
+
+GRAPHENE_IMPLEMENT_EXTERNAL_SERIALIZATION(graphene::protocol::asset_limitation_options)
+GRAPHENE_IMPLEMENT_EXTERNAL_SERIALIZATION(graphene::protocol::asset_limitation_object_create_operation::fee_parameters_type)
+GRAPHENE_IMPLEMENT_EXTERNAL_SERIALIZATION(graphene::protocol::asset_limitation_object_update_operation::fee_parameters_type)
+GRAPHENE_IMPLEMENT_EXTERNAL_SERIALIZATION(graphene::protocol::asset_limitation_object_create_operation)
+GRAPHENE_IMPLEMENT_EXTERNAL_SERIALIZATION(graphene::protocol::asset_limitation_object_update_operation)
