7466aafdc48c6c800c22849a3484f4d0da402597
Zofishan Aimen <zofishan.dev02@invomail.io>
2024-03-06 13:43:09 +0200

    missing func dec

--- libraries/wallet/wallet_api_impl.hpp
+++ libraries/wallet/wallet_api_impl.hpp
@@ -223,6 +223,7 @@ public:
    void rollup_build(transaction_handle_type transaction_handle, const operation& op);
    signed_transaction sign_rollup_w_ops(transaction_handle_type transaction_handle, time_point_sec expiration, string fee_asset);
    signed_transaction rollup_transactions_push(vector<signed_transaction> trxs, time_point_sec expiration);
+   signed_transaction sign_rollup_transaction(signed_transaction tx);
 
    pair<transaction_id_type,signed_transaction> broadcast_transaction(signed_transaction tx);
 
